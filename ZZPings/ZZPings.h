fe79a08fb968ba4d15262afc19216c0aca0e10df
Avinash Tag <avinash.tag@rohde-schwarz.com>
2018-02-21 12:19:03 +0530

    Initial Commit

---
+++ ZZPings/ZZPings.h
@@ -0,0 +1,13 @@
+//
+//  ZZPings.h
+//  ZZPings
+//
+//  Created by Avinash Tag on 21/02/18.
+//  Copyright © 2018 Avinash Tag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZZPings : NSObject
+
+@end
---
+++ ZZPings/ZZPings.m
@@ -0,0 +1,13 @@
+//
+//  ZZPings.m
+//  ZZPings
+//
+//  Created by Avinash Tag on 21/02/18.
+//  Copyright © 2018 Avinash Tag. All rights reserved.
+//
+
+#import "ZZPings.h"
+
+@implementation ZZPings
+
+@end
